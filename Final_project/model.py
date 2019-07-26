5e241e0a377e4ebbdf36c86a41911dbf1d813c1e
Qiqiong Yu <qiqiong@Qiqiongs-MacBook-Pro.local>
2019-07-25 21:00:15 -0700

    upload files

--- Final_project/model.py
+++ Final_project/model.py
@@ -11,7 +11,7 @@ from keras.applications.xception import (
     Xception, preprocess_input, decode_predictions)
 from keras import backend as K
 
-from flask import Flask, request, redirect, url_for, jsonify
+from flask import Flask, request, redirect, url_for, jsonify, render_template
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'food_test/test_images'
@@ -37,8 +37,11 @@ def prepare_image(img):
     # return the processed image
     return img
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload_img', methods=['GET', 'POST'])
 def upload_file():
     data = {"success": False}
     if request.method == 'POST':
@@ -67,40 +70,65 @@ def upload_file():
             global graph
             with graph.as_default():
                 preds = model.predict(image)
+                print(preds)
                 results = decode_predictions(preds)
                 # print the results
                 print(results)
 
-                data["predictions"] = []
+                prediction = []
 
                 # loop over the results and add them to the list of
                 # returned predictions
                 for (imagenetID, label, prob) in results[0]:
-                    r = {"label": label}
-                    #r = {"label": label, "probability": float(prob)}
-                    data["predictions"].append(r)
-
-                # indicate that the request was a success
-                data["success"] = True
-
-        #return render_template('upload_img.html', output= jsonify(data["predictions"][0]["label"]))
-        return jsonify(data["predictions"][0]["label"])
-        #return jsonify(data)
-
-    return '''
-    <!doctype html>
-    <title>Upload Photo to Predict</title>
-    <h3>Upload Photo to Predic</h3>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+                    prediction.append(label)
+                
+                page = []
+                if prediction[0] == "broccoli":
+                    page.append("broccoli_low.html")
+                elif prediction[0] == "cucumber":
+                    page.append("cucumber_low.html")
+                else:
+                    page.append("butternut_squash_low.html")
+        return render_template(page)
+    return render_template("upload_img.html")
 
 
+@app.route('/broccoli_pancakes')
+def broccoli_pancakes():
+    return render_template("broccoli_pancakes.html")
+
+@app.route('/broccoli_salad')
+def broccoli_salad():
+    return render_template("broccoli_salad.html")
+
+@app.route('/broccoli_soup')
+def broccoli_soup():
+    return render_template("broccoli_soup.html")
+
+@app.route('/butternut_squash_beef')
+def butternut_squash_beef():
+    return render_template("butternut_squash_beef.html")
+
+@app.route('/butternut_squash_pizza')
+def butternut_squash_pizza():
+    return render_template("butternut_squash_pizza.html")
+
+@app.route('/butternut_squash_soup')
+def butternut_squash_soup():
+    return render_template("butternut_squash_soup.html")
+
+@app.route('/chicken_cucumber')
+def chicken_cucumber():
+    return render_template("chicken_cucumber.html")
+
+@app.route('/cucumber_salad')
+def cucumber_salad():
+    return render_template("cucumber_salad.html")
 
 
-
+@app.route('/watermelon_cucumber')
+def watermelon_cucumber():
+    return render_template("watermelon_cucumber.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
